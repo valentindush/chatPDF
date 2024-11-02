4e7f870e0bf0fe13725cd52794f98962960fd328
dushvalentin <codesvalentin@gmail.com>
2024-11-02 20:52:22 +0200

    upd: initial GUI

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import streamlit as st
+
 def main():
-    print("hello")
+    st.set_page_config(
+        page_title="PDF Chat",
+        page_icon=":books:"
+    )
 
+    st.header("PDF Chat")
+    st.subheader("Ask questions about PDF documents")
+    st.chat_input("Ask questions")
 
+    with st.sidebar:
+        st.subheader("Documents")
+        st.file_uploader("Upload your PDFs here")
+        st.button("Process")
 
 if __name__ == "__main__":
     main()
